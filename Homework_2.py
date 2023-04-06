af2313fd8cec8c74f9a4197b5a32af4c7188c59c
Alex <129771453+SamAlexIT@users.noreply.github.com>
2023-04-07 00:20:52 +0300

    Add files via upload

---
+++ Homework_2.py
@@ -0,0 +1,49 @@
+a = 5
+b = 8
+c = 3
+a == b - c
+5 == 8 - 3
+print(a == b - c)
+print(5 == 8 - 3)
+print()
+
+week = None
+week = "(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)"
+print(week)
+print(type(week))
+print()
+
+today = "Thursday"
+print(today)
+print(type(today))
+print()
+
+today = "06-04-2023"
+print(today)
+print(type(today))
+print()
+
+print(15 / 100)
+print(24 - 12)
+print(True + (24 - 12))
+print()
+
+EPOCH_START = "1988-04-26"
+EPOCH_START = "34"
+EPOCH_START == 34
+print(EPOCH_START)
+print()
+
+EPOCH_START = 12
+print(EPOCH_START)
+
+My_name_is_Oleksandr = True
+True == ("print")
+
+
+def my_function():
+    print("Hello world!")
+    print('Hello world!')
+    print('My name is Oleksandr!')
+    print("My name is Oleksandr!")
+
