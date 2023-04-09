6779346a253edf4b276749f9184eb7d27ad9a475
Alex <129771453+SamAlexIT@users.noreply.github.com>
2023-04-10 00:25:02 +0300

    Add files via upload

---
+++ Homework_2.1.py
@@ -0,0 +1,75 @@
+a = 5
+b = 8
+c = 3
+a == b - c
+5 == 8 - 3
+a == 5
+print(a == b - c)
+print(5 == 8 - 3)
+print(a == 5)
+
+week = None
+week = "(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)"
+print(week)
+print(type(week))
+print()
+
+today = "Thursday"
+print(today)
+print(type(today))
+print()
+
+today = "06-04-2023"
+print(today)
+print(type(today))
+print()
+
+print(15 / 100)
+print(24 - 12)
+print(True + (24 - 12))
+print()
+
+EPOCH_START = "1988-04-26"
+EPOCH_START = "34"
+EPOCH_START == 34
+print(EPOCH_START)
+print()
+
+'EPOCH_START' + str(5 == 8 - 3)
+'EPOCH_START' + str(34)
+'EPOCH_START' + '(34)'
+print('EPOCH_START' + str(5 == 8 - 3))
+print('EPOCH_START' + str(34))
+print('EPOCH_START' + '(34)')
+
+
+
+
+"EPOCH_START\n Sunday, Sunday\n Monday\n Wednesday\n Thursday\n c = 3"
+print("EPOCH_START\n Sunday, Sunday\n Monday\n Wednesday\n Thursday\n c = 3")
+
+'5, 8, 3, "sdf", (5, 3), True, None'
+print('5, 8, 3, "sdf", (5, 3), True, None')
+
+'5, 8, 3, "sdf", (5, 3), True, None'
+print(('5, 8, 3, "sdf", (5, 3), True, None'))
+
+"5, 8, 3, Thursday\n c = 3, (5, 3)"
+print("5, 8, 3, Thursday\n c = 3, (5, 3)")
+
+{5, 8, 3, 5, 8, 3, 1, 0}
+print({5, 8, 3, 5, 8, 3, 1, 0})
+
+{2: 3, 4: 2, 2: 5}
+print({2: 3, 4: 2, 2: 5})
+
+List = [1, 2, 4, 4, 3, 3, 3, 6, 5]
+print("Original list ", List)
+[1, 2, 4, 4, 3, 3, 3, 6, 5]
+[1, 2, 4, 77, 3, 3, 3, 6, 5]
+tuple1 = (0, 1, 2, 3)
+tuple1[0] = 4
+print(tuple1)
+
+
+
