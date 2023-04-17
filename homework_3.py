d535ba9e8b613ff4606c25350c0f1264e5bf2f2f
Alex <arglexit@ukr.net>
2023-04-17 18:34:54 +0300

    home

---
+++ homework_3.py
@@ -0,0 +1,48 @@
+""" gfgwgsgf """
+
+
+def largest_number(arg_a, arg_b):
+    """ gfgwgsgf """
+    print("largest_number")
+
+    if arg_a >= arg_b:
+        return arg_a
+
+    return arg_b
+
+
+def lesser_number(arg_a, arg_b,arg_c):
+    """ gfgwgsgf """
+    print('lesser_number')
+
+    if arg_b >= arg_a <= arg_c:
+        return arg_a
+    if arg_b <= arg_c:
+        return arg_b
+    return arg_c
+
+
+def positive_number(arg_a):
+    """ gfgwgsgf """
+    print("positive_number")
+
+    if arg_a < 0:
+        return -arg_a
+    return arg_a
+
+
+def sum_number(arg_a, arg_b):
+    """ fiuwehiqewg """
+    print(arg_a + arg_b)
+
+
+def number(arg_a):
+    """ gfgwgsgf """
+    print("number")
+
+    if arg_a > 0:
+        print("positive")
+    elif arg_a < 0:
+        print("negative")
+    else:
+        print("zero")
