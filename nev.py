e96bdb92bf411348c1d2c56e0bd255d50cef2d67
Alex <129771453+SamAlexIT@users.noreply.github.com>
2023-05-25 00:23:42 +0300

    Add files via upload

---
+++ nev.py
@@ -0,0 +1,18 @@
+"""range"""
+
+
+def range_it():
+    """range_it"""
+    for i in range(5):
+        print(i)
+
+def ran_it():
+    """ran_it"""
+    for j in range(15):
+        print(j * 2)
+
+
+def ra_it():
+    """ra_it"""
+    for num in range(8):
+        print("Привіт" * num)
